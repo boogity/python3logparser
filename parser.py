0f1bafb1c4cb146c7ac594baae4c162c9984166a
wdell <harumph505@gmail.com>
2019-02-07 14:40:35 -0600

    added readme, migrating code to functions

--- parser.py
+++ parser.py
@@ -2,6 +2,12 @@ import os;
 import urllib.request;
 
 
+def lineCount(logs):
+	with open(logs) as x:
+		for i, l in enumerate(x):
+			pass
+	return i+1
+
 def checkForFile():
 	url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 	if os.path.exists("./http_access_log.txt") == False:
@@ -10,4 +16,8 @@ def checkForFile():
 	else:
 		print("what")
 
-checkForFile()
+def main():	
+	checkForFile()
+	print("Number of requests made:", lineCount("./http_access_log.txt"))
+
+main()
